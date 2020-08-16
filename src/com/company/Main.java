b39a86e95906a0ec9e3afc0f13ef96d2a6b55e17
xiewenhua <wenhua.xie@dhc.com.cn>
2020-08-16 20:58:49 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("hello");
+
+        ArrayList a =new ArrayList();
+        ArrayList a =new ArrayList();
+    }
+}
